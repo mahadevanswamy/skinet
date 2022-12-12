c41d3a12db18054c3960bb416dab56305dfd151e
Mahadevan Swamy <mahadevan.swamy@cfm-us.com>
2022-12-12 00:56:55 -0500

    End of section 6

--- Core/Specifications/BaseSpecifications.cs
+++ Core/Specifications/BaseSpecifications.cs
@@ -25,5 +25,29 @@ namespace Core.Specifications
     {
         Includes.Add(includeExpression);
     }
+
+    protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }    
+
+    protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+    {
+        OrderByDescending = orderByDescExpression;
+    }
+    public Expression<Func<T, object>> OrderBy {get; private set; }
+    public Expression<Func<T, object>> OrderByDescending {get; private set;}
+
+    public int Take {get; private set; }
+    public int Skip {get; private set; }
+    public bool IsPagingEnabled {get; private set; }
+    
+    protected void ApplyPaging(int skip, int take)
+    {
+      Skip = skip;
+      Take = take;
+      IsPagingEnabled = false;
+    }
+
   }
 }
